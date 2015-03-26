3f66540d8c80c594e49a7645164b4e9ead147b62
Joseph Pei <josephdpei@gmail.com>
2015-03-25 17:31:01 -0700

    Two tests are passing. Currently working on helper functions.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,8 +14,26 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  if (!asset){
+    res.writeHead(404, headers);
+    res.end('File not found!');
+  } else {
+    res.writeHead(200, headers);
+    fs.readFile(asset, function(err, data) {
+      if (err) throw err;
+      res.write(data, function() {
+        res.end();
+      });
+    });
+  }
 };
 
+exports.sendRequest = function() {
 
+};
+
+exports.sendRedirect = function() {
+
+};
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,45 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
+var getSite = function(pathName, response) {
+  // verify URL path
+  // serve http-helpers.serveAssets
+  // calls saveSite
+
+};
+
+var saveSite = function(request, response, callback) {
+  // Wait for POST data to be collected
+    // is it in sites.txt?
+      // if yes
+        // is it archive?
+          // if yes
+            // display archived page
+          // if no
+            // display loading page
+      // if no
+        // append to sites.txt
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // req.method, req.url
+  //We want to check the request url and make sure it is correct.
+  var pathName = url.parse(req.url).pathname;
+
+  if (req.method === "GET") {
+    var path = '/index.html';
+    if (pathName === path || pathName === '/'){
+      httpHelpers.serveAssets(res, archive.paths.siteAssets+'/index.html', function(){});
+    } else {
+      //is it in file already
+      //getSite(pathName, res);
+      httpHelpers.serveAssets(res);
+    }
+  } else if (req.method === "POST") {
+    //foo
+  }
+  //res.end(archive.paths.list);
 };
